69395e1e142badaad4f83bea6b0959cfdf483616
Brian Calendine <xenspidey@gmail.com>
2022-04-07 09:27:28 -0400

    Added main() for loose calcs

--- main.py
+++ main.py
@@ -59,35 +59,32 @@ def run_calc(cfm, db, wb, elev):
     calculations["Mass Flow Rate, water vapor"] = mfr_wv
     mfr_ma = mfr_dry + mfr_wv
     calculations["Mass Flow Rate, moist air"] = mfr_ma
-    # print(calculations)
-    if db < 0 and db > 120:
-        error_handler("Dry Bulb temp is out of range")
-
-    if wb < 0 and db > 120:
-        error_handler("Wet Bulb temp is out of range")
     return calculations
 
 
-air_flow_1 = run_calc(1000, 96, 75, 1000)
-atm_pres = air_flow_1["Atmospheric Pressure"]
-h_1 = air_flow_1["Enthalpy"]
-humidity_ratio_1 = air_flow_1["Humidity Ratio"]
-mfr_dry_1 = air_flow_1["Mass Flow Rate, dry air"]
-mfr_wv_1 = air_flow_1["Mass Flow Rate, water vapor"]
-mfr_ma_1 = air_flow_1["Mass Flow Rate, moist air"]
-air_flow_2 = run_calc(100, 75, 62.5, 1000)
-h_2 = air_flow_2["Enthalpy"]
-humidity_ratio_2 = air_flow_2["Humidity Ratio"]
-mfr_dry_2 = air_flow_2["Mass Flow Rate, dry air"]
-mfr_wv_2 = air_flow_2["Mass Flow Rate, water vapor"]
-mfr_ma_2 = air_flow_2["Mass Flow Rate, moist air"]
-mfr_dry_m = mfr_dry_1 + mfr_dry_2
-mfr_wv_m = mfr_wv_1 + mfr_wv_2
-mfr_ma_m = mfr_dry_m + mfr_wv_m
-h_m = ((h_1 * mfr_ma_1) + (h_2 * mfr_ma_2)) / mfr_ma_m
-wb_m = psychro.psychro_wb(db_m, h_m, atm_pres)
-humidity_ratio_m = ((humidity_ratio_1 * mfr_ma_1) +
-                    (humidity_ratio_2 + mfr_ma_2)) / mfr_ma_m
-air_flow_m = run_calc_m(db_m, wb_m, atm_pres)
-print(wb_m)
-# print(air_flow_1, air_flow_2)
+def main():
+    air_flow_1 = run_calc(1000, 96, 75, 1000)
+    atm_pres = air_flow_1["Atmospheric Pressure"]
+    h_1 = air_flow_1["Enthalpy"]
+    humidity_ratio_1 = air_flow_1["Humidity Ratio"]
+    mfr_dry_1 = air_flow_1["Mass Flow Rate, dry air"]
+    mfr_wv_1 = air_flow_1["Mass Flow Rate, water vapor"]
+    mfr_ma_1 = air_flow_1["Mass Flow Rate, moist air"]
+    air_flow_2 = run_calc(100, 75, 62.5, 1000)
+    h_2 = air_flow_2["Enthalpy"]
+    humidity_ratio_2 = air_flow_2["Humidity Ratio"]
+    mfr_dry_2 = air_flow_2["Mass Flow Rate, dry air"]
+    mfr_wv_2 = air_flow_2["Mass Flow Rate, water vapor"]
+    mfr_ma_2 = air_flow_2["Mass Flow Rate, moist air"]
+    mfr_dry_m = mfr_dry_1 + mfr_dry_2
+    mfr_wv_m = mfr_wv_1 + mfr_wv_2
+    mfr_ma_m = mfr_dry_m + mfr_wv_m
+    h_m = ((h_1 * mfr_ma_1) + (h_2 * mfr_ma_2)) / mfr_ma_m
+    wb_m = psychro.psychro_wb(db_m, h_m, atm_pres)
+    humidity_ratio_m = ((humidity_ratio_1 * mfr_ma_1) +
+                        (humidity_ratio_2 + mfr_ma_2)) / mfr_ma_m
+    air_flow_m = run_calc_m(db_m, wb_m, atm_pres)
+    print(wb_m)
+
+
+main()
